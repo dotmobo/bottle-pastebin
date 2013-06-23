cf126ae25dcc05a79c7ddacd0a34560aaf0b3cc5
Morgan <morgan.bohn@unistra.fr>
2013-06-23 19:49:34 +0200

    add unit tests for database

--- bottle_pastebin/test.py
+++ bottle_pastebin/test.py
@@ -2,6 +2,9 @@
 import unittest
 from utils import encryptAES256, decryptAES256, encryptSHA256, dateLimit
 import datetime
+from database import Valid, Text
+import peewee
+from playhouse.test_utils import test_database
 
 class UtilsTest(unittest.TestCase):
     """ unit test for utils """
@@ -35,5 +38,33 @@ class UtilsTest(unittest.TestCase):
         datestr = datetime.datetime.strftime(datelimit, "%Y-%m-%d %Hh:%Mm:%Ss")
         self.assertEqual("2013-01-02 00h:00m:00s", datestr)
         
+class DatabaseTest(unittest.TestCase):
+    """ database unit test """
+
+    def setUp(self):
+        self.test_db = peewee.SqliteDatabase(':memory:')
+        
+    def test_data(self):
+        """ test valid """
+        with test_database(self.test_db, (Valid, Text)):
+            self.valid = Valid(description="One day", duration=86400000)
+            self.valid.save()
+            self.text = Text(text="my encrypted text", 
+                code="e1f7785845cac9d0719b902eaef13bdf7fb9442a368c1fadac2570c1f6579f42",valid=self.valid)
+            self.text.save()
+    
+            valid_getted = Valid.get(id = 1) 
+            self.assertEqual(valid_getted.description, "One day")
+            self.assertEqual(valid_getted.duration, 86400000)
+            
+            text_getted = Text.get(id = 1)
+            self.assertEqual(text_getted.text, "my encrypted text")
+            self.assertEqual(text_getted.code, "e1f7785845cac9d0719b902eaef13bdf7fb9442a368c1fadac2570c1f6579f42")
+            self.assertEqual(text_getted.valid.description, "One day")
+            self.assertEqual(text_getted.valid.duration, 86400000)
+  
         
         
+
+        
+    
